7c60b1fc1d460e8b05d058cf7b57cd37905684bc
Ashwin Naren <arihant2math@gmail.com>
2026-04-27 15:37:55 -0700

    cleanup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   site: 'https://www.projectpixelorbital.com',
   output: 'static',
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
