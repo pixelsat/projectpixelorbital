52cbb52709c995e3ad23ceba72afe711d0d76140
Ashwin Naren <arihant2math@gmail.com>
2026-06-10 23:08:34 -0700

    dm if you have questions :sob:

--- src/components/PrismLanding.svelte
+++ src/components/PrismLanding.svelte
@@ -50,12 +50,12 @@
 
     const team = [
         { name: "Weston Yoo", role: "Overall team lead", image: weston },
-        { name: "Vinayak Vikram", role: "Electrical lead · software · structural", image: vinayak },
-        { name: "Aadish Verma", role: "Comms lead · software", image: aadish },
-        { name: "Victoria McLeod", role: "Structural Lead", image: victoria },
-        { name: "Grace Zheng", role: "Structural · media", image: grace },
         // TODO: add link to https://arihant2math.github.io/ back
         { name: "Ashwin Naren", role: "Software BDFL/Lead · electrical · structural", image: ashwin },
+        { name: "Aadish Verma", role: "Comms lead · software", image: aadish },
+        { name: "Vinayak Vikram", role: "Electrical lead · software · structural", image: vinayak },
+        { name: "Victoria McLeod", role: "Structural Lead", image: victoria },
+        { name: "Grace Zheng", role: "Structural · media", image: grace },
         { name: "Daphne F", role: "Outreach lead", initials: "DF" },
         { name: "Nathan Muruganantham · ADCS", role: "Electrical", initials: "NM" },
         { name: "Shiv Sareen", role: "Outreach · funding", initials: "SS" },
