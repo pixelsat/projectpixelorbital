822c56f77013afd6b57545b834ecdc26491be981
Ashwin Naren <arihant2math@gmail.com>
2026-07-12 12:49:33 -0500

    set up katex better

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,23 @@
+import { unified } from "@astrojs/markdown-remark";
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import rehypeKatex from "rehype-katex";
+import remarkMath from "remark-math";
 
 export default defineConfig({
   site: "https://www.projectpixelorbital.com",
   output: "static",
   markdown: {
-    shikiConfig: {
-      themes: {
-        light: "github-light",
-        dark: "github-dark",
+    processor: unified({
+      remarkPlugins: [remarkMath],
+      rehypePlugins: [rehypeKatex],
+      shikiConfig: {
+        themes: {
+          light: "github-light",
+          dark: "github-dark",
+        },
       },
-    },
+    }),
   },
   vite: {
     plugins: [tailwindcss()],
