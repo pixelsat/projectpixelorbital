551e34e926000287f9e3e4000e686d16ef5a31a0
Ashwin Naren <arihant2math@gmail.com>
2026-07-12 11:34:42 -0500

    latex rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
+import { unified } from "@astrojs/markdown-remark";
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import rehypeKatex from "rehype-katex";
+import remarkMath from "remark-math";
 
 export default defineConfig({
   site: "https://www.projectpixelorbital.com",
   output: "static",
   markdown: {
+    processor: unified({
+      remarkPlugins: [remarkMath],
+      rehypePlugins: [rehypeKatex],
+    }),
     shikiConfig: {
       themes: {
         light: "github-light",
