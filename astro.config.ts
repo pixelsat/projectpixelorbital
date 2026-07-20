7bc78565e7d8897b9ee45e3f22444d9736ae4cc6
Ashwin Naren <arihant2math@gmail.com>
2026-07-20 11:32:04 -0700

    initial

--- astro.config.ts
+++ astro.config.ts
@@ -13,10 +13,7 @@ export default defineConfig({
       rehypePlugins: [rehypeKatex],
     }),
     shikiConfig: {
-      themes: {
-        light: "github-light",
-        dark: "github-dark",
-      },
+      theme: "github-dark",
     },
   },
   vite: {
